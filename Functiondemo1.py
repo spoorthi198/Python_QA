93a46a51237e7d3f00cc040ce467f9a53f451e2c
spoorthi <spurti20oct@gmail.com>
2020-12-04 11:56:06 +0530

    new prog *agrs and **kwargs eg.py added

---
+++ Functiondemo1.py
@@ -0,0 +1,36 @@
+# usage of *args and **kwargs
+
+# *args => results in tuple
+
+def print_name(*args):
+    print(args)
+
+print_name("spoorthi","raj","jnanika")
+
+def get_sum(*args):
+    print(sum(args))
+
+get_sum(20,45,67,78)
+
+def get_min(*args):
+    print(min(args))
+get_min(23,5,87,9,12)
+
+
+def get_max(*args):
+    print(max(args))
+get_max(23,4,768,876,345)
+
+# *kwargs => results in tuple
+
+def get_names(**kwargs):
+    print(kwargs)
+
+get_names(name = "spoorthi",address="bangalore",phone= 5997)
+
+def hello_world(*args,**kwargs):
+    print(args)
+    print((kwargs))
+    print(kwargs["address"])
+
+hello_world(10,23,46,name="spoorthi",address="bangalore",phone=835098)
