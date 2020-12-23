b7fc00fbf72d0fd67f90babf68423bebdce88512
spoorthi <spurti20oct@gmail.com>
2020-12-23 15:02:42 +0530

    new prog created class

--- sum.py
+++ sum.py
@@ -1,22 +1,28 @@
-class Summing:
-    def twosum(self, nums,target):
-        compliments = {}
-        result = []
+# given array retun the indicies of two number which computes to target
 
-        for index, num in enumerate(nums):
-            if compliments.get(num) is None:
-                compliments[target-num] = index
-            else:
-                result =[compliments[num],index]
-                break
-        return result
+class Summing:
+
+  def two_Sum(self,nums,target):
+      complements = {}
+      result = []
+
+      for index,num in enumerate(nums):
+          if complements.get(num) is None:
+              complements[target-num] = index
+          else:
+              result = [complements[num],index]
+              break
+      return result
+
+
+
 
 
 l=Summing()
-#res = l.twosum([1,2,3,4],4)
+res = l.two_Sum([1,2,3,4],4)
 
-res=l.twosum([2,7,11,15],9)
-res1=l.twosum([2,7,11,15],26)
+res1=l.two_Sum([2,7,11,15],9)
+res2=l.two_Sum([2,7,11,15],26)
 #{24:0,19:1,15:2}
 # 15 exists so res=[2,3]
 
@@ -24,7 +30,7 @@ res1=l.twosum([2,7,11,15],26)
 
 
 
-print(res1)
+print(res2)
 
 
 
